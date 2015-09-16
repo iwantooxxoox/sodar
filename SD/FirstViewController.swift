b6f6f5929e690db37d290614cd44eb89a3f5293d
Victor Wang <iwantooxxoox@gmail.com>
2015-09-16 17:59:39 +0800

    Initial Commit

---
+++ SD/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SD
+//
+//  Created by Mustang Design on 2015/9/16.
+//  Copyright (c) 2015年 teamecodenect. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
