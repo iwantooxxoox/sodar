c95efa88a72e1bb84728bf7042e921a61a7f38c4
Victor Wang <iwantooxxoox@gmail.com>
2015-09-20 22:30:38 +0800

    modified

---
+++ SD/ExploreViewController.swift
@@ -0,0 +1,71 @@
+//
+//  FirstViewController.swift
+//  SD
+//
+//  Created by Mustang Design on 2015/9/16.
+//  Copyright (c) 2015年 teamecodenect. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: PFQueryTableViewController {
+
+    override init(style: UITableViewStyle, className: String!)
+    {
+        super.init(style: style, className: className)
+        
+        self.pullToRefreshEnabled = true
+        self.paginationEnabled = false
+        self.objectsPerPage = 25
+        
+        self.parseClassName = className
+    }
+    
+    required init(coder aDecoder:NSCoder)
+    {
+        fatalError("NSCoding not supported")  
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func queryForTable() -> PFQuery {
+        var query:PFQuery = PFQuery(className:self.parseClassName!)
+        
+        if(objects?.count == 0)
+        {
+            query.cachePolicy = PFCachePolicy.CacheThenNetwork
+        }
+        
+        query.orderByAscending("name")
+        
+        return query
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell? {
+        
+        let cellIdentifier:String = "Cell"
+        
+        var cell:PFTableViewCell? = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? PFTableViewCell
+        
+        if(cell == nil) {
+            cell = PFTableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellIdentifier)
+        }
+        
+        if let pfObject = object {
+            cell?.textLabel?.text = pfObject["name"] as? String
+        }
+        
+        return cell;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
